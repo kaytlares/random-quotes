357bb37ae49fe0f51477517367527945ec71ffdb
kaytlares <97468630+kaytlares@users.noreply.github.com>
2022-02-02 17:43:04 -0500

    Created Quote App

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # random-quotes
 Simple node.js application for a class that prints random quotes.
+
+## Favorite quotation 
+
+"Some people want it to happen, some wish it would happen, others make it happen." 
+
+– Michael Jordan.
---
+++ index.js
@@ -0,0 +1,6 @@
+let quotes = [`"Some people want it to happen, some wish it would happen, others make it happen." -Michael Jordan.`,
+`"To make no mistakes is not in the power of man; but from their errors and mistakes the wise and good learn wisdom for the future."-Plutarch`,
+`"You can only become truly accomplished at something you love."-Maya Angelou`]
+
+let index = Math.floor ((3 * Math.random()))
+console.log(quotes[index])
